4a6bd80f32637db14ef97f2de831638d38066b3a
aaaa <>
2025-07-19 17:23:40 +0900

    Player作成

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+        }
+    }
+}
